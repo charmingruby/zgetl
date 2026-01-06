fed18101b58e04f09a5499cc47e43dde649a1db5
charmingruby <mrrubycanal3105@gmail.com>
2026-01-05 21:31:36 -0300

    feat: adds a bigger dataset

--- z/src/UId.zig
+++ z/src/UId.zig
@@ -5,6 +5,7 @@ pub const UId = @This();
 
 allocator: std.mem.Allocator,
 ids: std.ArrayList([]const u8),
+mutex: std.Thread.Mutex,
 
 pub fn init(allocator: std.mem.Allocator) !*UId {
     const self = try allocator.create(UId);
@@ -12,12 +13,16 @@ pub fn init(allocator: std.mem.Allocator) !*UId {
     self.* = .{
         .allocator = allocator,
         .ids = try std.ArrayList([]const u8).initCapacity(allocator, 0),
+        .mutex = .{},
     };
 
     return self;
 }
 
 pub fn generateId(self: *UId) ![]const u8 {
+    self.mutex.lock();
+    defer self.mutex.unlock();
+
     const id = uuid.v7.new();
 
     const id_str = std.fmt.allocPrint(self.allocator, "{d}", .{id}) catch unreachable;
